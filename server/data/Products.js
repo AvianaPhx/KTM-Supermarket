5bd1118be93e2be8f754a00b11cd29cd917f6b5f
AvianaPhx <avianaphoenix@gmail.com>
2024-11-08 11:13:15 +0545

    Backend + Database
    Ensuring the connection between the express node.js and mySQL

---
+++ server/data/Products.js
@@ -0,0 +1,17 @@
+const products = [
+  {
+    id: '1',
+    name: "Aviana Phoenix",
+    price: 20,
+  },
+  {
+    name: "Nathen",
+    price: 27,
+  },
+  {
+    name: "krisha",
+    price: 21
+  },
+]
+
+module.exports = products;
--- server/server.js
+++ server/server.js
@@ -1,16 +1,48 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const dotenv = require("dotenv");
+const products = require("./data/Products")
+dotenv.config()
 const corsOptions = {
   origin: ["http://localhost:5173"]
 };
+const PORT = process.env.PORT;
+const mysql = require('mysql');
+
 
 app.use(cors(corsOptions));
 
-app.get("/api", (req, res) => {
-  res.json({ "fruits": ["Aviana", "Phoenix", "Test"] });
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "ktm_supermarket"
+})
+
+app.get("/product", (req, res) => {
+  const sql = "SELECT * FROM products";
+  db.query(sql, (err, data) => {
+    if(err) return res.json("Error");
+    return res.json(data);
+  })
+})
+
+
+app.listen(PORT || 9000, () => {
+  console.log(`server listening on port ${PORT}`);
 });
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
-});
+
+
+
+// API TEST PRODUCT
+// app.get("/server/products", (req, res) => {
+//   res.json(products)
+// });
+
+// app.get("/server/products/:id", (req, res) => {
+//   const product = products.find((product)=>product.id == req.params.id)
+//   res.json(product)
+// });
